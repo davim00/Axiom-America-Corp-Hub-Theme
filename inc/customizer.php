190e99a91a75f93ff37d856e4141af64630b1fb0
Matthew Davis <matt.davis0207@gmail.com>
2016-11-04 17:06:42 -0400

    Created options for hero section

--- inc/customizer.php
+++ inc/customizer.php
@@ -14,6 +14,7 @@ function axiom_america_customize_register( $wp_customize ) {
 	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
 	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
 	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+	$wp_customize->remove_section('header_image');
 }
 add_action( 'customize_register', 'axiom_america_customize_register' );
 
@@ -24,3 +25,205 @@ function axiom_america_customize_preview_js() {
 	wp_enqueue_script( 'axiom_america_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
 }
 add_action( 'customize_preview_init', 'axiom_america_customize_preview_js' );
+
+/**
+ * Custom Customizer controls and options.
+ */
+ axiomamerica_Kirki::add_section( 'hero_image', array(
+   'title' => __( 'Hero Section' ),
+   'description' => __( 'Show a hero section on the home page.' ),
+	 'capability'	=> 'edit_theme_options',
+   'priority' => 61
+ ) );
+
+axiomamerica_Kirki::add_field( 'hero_image_toggle', array(
+	'settings' => 'hero_image_toggle',
+	'label'    => __( 'Turn the hero section on or off', 'axiomamerica' ),
+	'section'  => 'hero_image',
+	'type'     => 'switch',
+	'default'  => '0',
+	'priority' => 10
+) );
+
+axiomamerica_Kirki::add_field( 'hero_image_background_color', array(
+	'settings' => 'hero_image_background_color',
+	'label'    => __( 'Select a solid background color', 'axiomamerica' ),
+	'section'  => 'hero_image',
+	'type'     => 'color',
+	'default'  => '#50526d',
+	'priority' => 20,
+	'active_callback' => array(
+		array(
+			'setting'  => 'hero_image_toggle',
+			'operator' => '==',
+			'value'    => '1',
+		),
+	),
+) );
+
+axiomamerica_Kirki::add_field( 'hero_image_background_image', array(
+	'settings' => 'hero_image_background_image',
+	'label'    => __( 'Select a background image', 'axiomamerica' ),
+	'section'  => 'hero_image',
+	'type'     => 'image',
+	'default'  => '',
+	'priority' => 21,
+	'active_callback' => array(
+		array(
+			'setting'  => 'hero_image_toggle',
+			'operator' => '==',
+			'value'    => '1',
+		),
+	),
+) );
+
+axiomamerica_Kirki::add_field( 'hero_image_foreground_image', array(
+	'settings' => 'hero_image_foreground_image',
+	'label'    => __( 'Select a foreground image', 'axiomamerica' ),
+	'description' => __( 'PNG or SVG format images with transparent backgrounds work best', 'axiomamerica' ),
+	'section'  => 'hero_image',
+	'type'     => 'image',
+	'default'  => '',
+	'priority' => 22,
+	'active_callback' => array(
+		array(
+			'setting'  => 'hero_image_toggle',
+			'operator' => '==',
+			'value'    => '1',
+		),
+	),
+) );
+
+axiomamerica_Kirki::add_field( 'hero_image_title', array(
+	'settings' => 'hero_image_title',
+	'label'    => __( 'Text for the title of the hero section', 'axiomamerica' ),
+	'section'  => 'hero_image',
+	'type'     => 'text',
+	'default'  => esc_attr__( 'A Remarkable Website', 'axiomamerica' ),
+	'priority' => 30,
+	'active_callback' => array(
+		array(
+			'setting'  => 'hero_image_toggle',
+			'operator' => '==',
+			'value'    => '1',
+		),
+	),
+) );
+
+axiomamerica_Kirki::add_field( 'hero_image_button_01', array(
+	'settings' => 'hero_image_button_01',
+	'label'    => __( 'Show one button under the title', 'axiomamerica' ),
+	'section'  => 'hero_image',
+	'type'     => 'switch',
+	'default'  => '0',
+	'choices' => array(
+    'on'  => esc_attr__( 'Show', 'axiomamerica' ),
+    'off' => esc_attr__( 'Hide', 'axiomamerica' )
+	),
+	'priority' => 40,
+	'active_callback' => array(
+		array(
+			'setting'  => 'hero_image_toggle',
+			'operator' => '==',
+			'value'    => '1',
+		),
+	),
+) );
+
+axiomamerica_Kirki::add_field( 'hero_image_button_01_text', array(
+	'settings' => 'hero_image_button_01_text',
+	'label'    => __( 'Button text', 'axiomamerica' ),
+	'section'  => 'hero_image',
+	'type'     => 'text',
+	'default'  => esc_attr__( 'Button 1', 'axiomamerica' ),
+	'priority' => 41,
+	'active_callback' => array(
+		array(
+			'setting'  => 'hero_image_button_01',
+			'operator' => '==',
+			'value'    => '1',
+		),
+	),
+) );
+
+axiomamerica_Kirki::add_field( 'hero_image_button_01_link', array(
+	'settings' => 'hero_image_button_01_link',
+	'label'    => __( 'URL or page for the button link', 'axiomamerica' ),
+	'section'  => 'hero_image',
+	'type'     => 'text',
+	'default'  => '#',
+	'priority' => 42,
+	'active_callback' => array(
+		array(
+			'setting'  => 'hero_image_button_01',
+			'operator' => '==',
+			'value'    => '1',
+		),
+	),
+) );
+
+axiomamerica_Kirki::add_field( 'hero_image_button_02', array(
+	'settings' => 'hero_image_button_02',
+	'label'    => __( 'Show second button under the title', 'axiomamerica' ),
+	'section'  => 'hero_image',
+	'type'     => 'switch',
+	'default'  => '0',
+	'choices' => array(
+    'on'  => esc_attr__( 'Show', 'axiomamerica' ),
+    'off' => esc_attr__( 'Hide', 'axiomamerica' )
+	),
+	'priority' => 50,
+	'active_callback' => array(
+		array(
+			'setting'  => 'hero_image_toggle',
+			'operator' => '==',
+			'value'    => '1',
+		),
+	),
+) );
+
+axiomamerica_Kirki::add_field( 'hero_image_button_02_text', array(
+	'settings' => 'hero_image_button_02_text',
+	'label'    => __( 'Button text', 'axiomamerica' ),
+	'section'  => 'hero_image',
+	'type'     => 'text',
+	'default'  => esc_attr__( 'Button 1', 'axiomamerica' ),
+	'priority' => 51,
+	'active_callback' => array(
+		array(
+			'setting'  => 'hero_image_button_02',
+			'operator' => '==',
+			'value'    => '1',
+		),
+	),
+) );
+
+axiomamerica_Kirki::add_field( 'hero_image_button_02_link', array(
+	'settings' => 'hero_image_button_02_link',
+	'label'    => __( 'URL or page for the button link', 'axiomamerica' ),
+	'section'  => 'hero_image',
+	'type'     => 'text',
+	'default'  => '#',
+	'priority' => 52,
+	'active_callback' => array(
+		array(
+			'setting'  => 'hero_image_button_02',
+			'operator' => '==',
+			'value'    => '1',
+		),
+	),
+) );
+
+axiomamerica_Kirki::add_field( 'hero_image_visibility', array(
+	'settings'		=> 'hero_image_visibility',
+	'label'			=> __( 'Visibility', 'axiomamerica' ),
+	'description'	=> __( 'Select where the hero section should be visible.', 'axiomamerica' ),
+	'section'		=> 'hero_image',
+	'type'			=> 'select',
+	'choices'		=> array(
+		'homepage'	=> __( 'Homepage', 'axiomamerica' ),
+		'frontpage'	=> __( 'Front Page', 'axiomamerica' )
+	),
+	'default'		=> 'homepage',
+	'priority'  => 60
+) );
